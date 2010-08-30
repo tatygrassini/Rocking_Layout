b07ee366f057b13bbe7f8bd396b5b4ecf2cf092e
tatygrassini <tatygrassini@gmail.com>
2010-08-30 23:38:21 +0200

    trying sifr

--- js/sIFR/sifr-config.js
+++ js/sIFR/sifr-config.js
@@ -1,11 +1,11 @@
 
-var rockwell = { src: 'js/sIFR/rockwell.swf' };
+var rockwell = { src: ' rockwell.swf' };
 
 
 sIFR.activate(rockwell);
 
 
 sIFR.replace(rockwell, {
-  selector: 'h3.flash', 
+  selector: 'h3.flash', 'h4.flash',
   css: '.sIFR-root { background-color: #191919; color: #e18610; font-size: 30px; text-transform: uppercase; margin-bottom: 9px;}'
 });
